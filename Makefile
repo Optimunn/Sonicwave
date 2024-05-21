ab058549e8b002cdd7e65563a0bf921dfb36d5b6
Optimum <ilushenko04@gmail.com>
2024-05-21 14:42:38 +0300

    modified:   Makefile 	modified:   attinybz.asm 	deleted:    attinybz.hex

--- Makefile
+++ Makefile
@@ -3,13 +3,13 @@ MCU       = attiny10
 FILE      = attinybz
 PGMR      = usbasp
 BITCLOCK  = 125kHz
-
+COMPILER_PATH = /opt/homebrew/Cellar/avra
 all: compile clean upload
 
 upload:
 	avrdude -p $(MCU) -c $(PGMR) -B $(BITCLOCK) -U flash:w:$(FILE).hex
 compile:
-	avra --includepath /opt/homebrew/Cellar/avra/1.4.2/include/avr \
+	avra --includepath $(COMPILER_PATH)/1.4.2/include/avr \
 	-o $(FILE).hex $(FILE).asm
 clean:
 	rm $(FILE).eep.hex $(FILE).obj
--- attinybz.asm
+++ attinybz.asm
@@ -8,9 +8,9 @@
     .def sys   = r21
     .def temp  = r20 
     .def regst = r22 
-    .def tone  = r23 ;memory
-    .def count = r24 ;memory
-    .def flag  = r25 ;memory
+    .def tone  = r23                ;memory
+    .def count = r24                ;memory
+    .def flag  = r25                ;memory
 
     .dseg
     .cseg
@@ -19,45 +19,44 @@
     .org 0x004 
     rjmp TIM0_OVF
 RESET:
-    ldi temp, high(ramend); stek init ->
+    ldi temp, high(ramend)          ; stek init ->
     out sph, temp 
     ldi temp, low(ramend)
-    out spl, temp ; <- *
+    out spl, temp                   ; <- *
 
-    ldi temp, 0b110; set ports 1 and 2 to out
+    ldi temp, 0b110                 ; set ports 1 and 2 to out
     out DDRB, temp 
-    ldi temp, 0b001; set input pulup on pin 0
+    ldi temp, 0b001                 ; set input pulup on pin 0
     out PUEB, temp
 
-    ldi temp, 0b00000001; set clock to 1:1
+    ldi temp, 0b00000001            ; set clock to 1:1
     out TCCR0B, temp
-    ldi temp, 0b1; inisialize interrupts
+    ldi temp, 0b1                   ; inisialize interrupts
     out TIMSK0, temp
     out TIFR0, temp 
 
-    ldi tone, 140; set start tone to 140
-    
-    sei; resolving interrupts
+    ldi tone, 140                   ; set start tone to 140
+    sei                             ; resolving interrupts
 LOOP:
-    sbis PINB0, 0; if pin0 == 1 skip the following command
+    sbis PINB0, 0                   ; if pin0 == 1 skip the following command
     sbr flag, 1 
 
-    cpi count, 150; if count == 150 jump to Ps
+    cpi count, 150                  ; if count == 150 jump to Ps
     brsh Branch
     rjmp LOOP
 Branch:  
     clr count
-    sbrc flag, 1; if bit1 in flag == low jump to vaeup
-    breq vaveup   ;SBRS - установлен
-    sbrs flag, 1; fi bit1 in flag == hight jump to vavedn
+    sbrc flag, 1                    ; if bit1 in flag == low jump to vaeup
+    breq vaveup                     ; SBRS - установлен
+    sbrs flag, 1                    ; fi bit1 in flag == hight jump to vavedn
     breq vavedn
 vaveup:
-    inc tone; tone++
+    inc tone                        ; tone++
     cpi tone, 180
     breq rvave
     rjmp LOOP
 vavedn:
-    dec tone; tone--
+    dec tone                        ; tone--
     cpi tone, 130
     breq rvave
     rjmp LOOP
@@ -70,10 +69,10 @@ rvave:
     eor tree, sys
     out PORTB, tree 
     rjmp LOOP
-TIM0_OVF:; interrupt vector
+TIM0_OVF:                           ; interrupt vector
     cli
     inc count
-    sbrc flag, 0; if bit0 in flag == 1 jump to PIN
+    sbrc flag, 0                    ; if bit0 in flag == 1 jump to PIN
     rcall PIN
 
     ldi temp, 255 
